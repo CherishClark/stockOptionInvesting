346085fca0462cb730830069cd94f1f8d55c94e7
cherish.clark <cherish.g.clark@gmail.com>
2018-05-12 18:46:22 -0600

    creating Employee Record

---
+++ src/main/java/EmployeeRecord.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeRecord {
+    EventInfo eventInfo;
+    List<Event> eventList = new ArrayList<>();
+
+    public EmployeeRecord(EventInfo eventInfo) {
+        this.eventList = eventList;
+    }
+
+    public List<Event> getEventList() {
+        return eventList;
+    }
+
+
+}
