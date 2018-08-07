56514a538d089e86fb07737f95491f52566276a7
CherishClark <cherish.g.clark@gmail.com>
2018-08-07 06:47:59 -0600

    removing unused method

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -74,14 +74,4 @@ public class Event {
     public void increaseEventAmount(BigDecimal amount) {
 
     }
-
-    public Event createEvent(String eventString, String fileDelimiter) throws Exception {
-        try {
-            return new Event.Builder().build();
-        } catch (Exception e) {
-            throw new Exception(eventString + "Event could not be created");
-        }
-    }
-
-
 }
